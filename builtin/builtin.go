1de984699e3119fad3782157530dee021d764f24
Andrew Couch <andy@couchand.com>
2014-11-19 09:38:19 -0500

    maps

--- builtin/builtin.go
+++ builtin/builtin.go
@@ -46,6 +46,68 @@ func list(ps []types.Value) types.Value {
     return types.Cons(ps[0], list(ps[1:]))
 }
 
+func length(ps []types.Value) types.Value {
+    if len(ps) != 1 {
+        panic("len expectes one parameter only")
+    }
+    return types.Int(ps[0].Len())
+}
+
+func mapp(ps []types.Value) types.Value {
+    if len(ps) % 2 != 0 {
+        panic("map expects even number of params")
+    }
+    pairs := make([]types.Value, len(ps) / 2)
+    for i := 0; i < len(ps); i += 2 {
+        if !ps[i].IsAtom() {
+            panic("map expects atoms for keys")
+        }
+
+        pairs[i/2] = types.Cons(ps[i], ps[i+1])
+    }
+    return list(pairs)
+}
+
+func get(ps []types.Value) types.Value {
+    if len(ps) != 2 {
+        panic("get expects two parameters")
+    }
+    if !ps[1].IsAtom() {
+        panic("get expects atom for key")
+    }
+    if ps[0].IsNil() {
+        return types.Nil()
+    }
+    if !ps[0].IsProperList() {
+        panic("get expects map")
+    }
+    pair := ps[0].CarVal()
+    if pair.CarVal().AtomVal() == ps[1].AtomVal() {
+        return pair.CdrVal()
+    }
+    return get([]types.Value{ ps[0].CdrVal(), ps[1] })
+}
+
+func set(ps []types.Value) types.Value {
+    if len(ps) != 3 {
+        panic("set expects two parameters")
+    }
+    if !ps[1].IsAtom() {
+        panic("set expects atom for key")
+    }
+    if ps[0].IsNil() {
+        return types.Cons(types.Cons(ps[1], ps[2]), types.Nil())
+    }
+    if !ps[0].IsProperList() {
+        panic("set expects map")
+    }
+    pair := ps[0].CarVal()
+    if pair.CarVal().AtomVal() == ps[1].AtomVal() {
+        return types.Cons(types.Cons(ps[1], ps[2]), ps[0].CdrVal())
+    }
+    return types.Cons(pair, set([]types.Value{ ps[0].CdrVal(), ps[1], ps[2] }))
+}
+
 var Builtins = map[string](func([]types.Value)types.Value){
     "+": sum,
     "*": mul,
@@ -53,4 +115,8 @@ var Builtins = map[string](func([]types.Value)types.Value){
     "car": car,
     "cdr": cdr,
     "list": list,
+    "len": length,
+    "map": mapp,
+    "get": get,
+    "set": set,
 }
